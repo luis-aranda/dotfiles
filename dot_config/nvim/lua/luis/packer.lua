dced9df3acfdb48b9fa3f19fe5ecb258f3c38acc
Luis Aranda <luis@Skynet>
2022-10-06 09:42:59 -0500

    Add nvim configurations
    Add nvim as difftool on chezmoi
    Add picker on nvim for fuzzy find on current file

--- dot_config/nvim/lua/luis/packer.lua
+++ dot_config/nvim/lua/luis/packer.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function()
 	--Lualine
 	use {
 		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+		requires = { 'kyazdani42/nvim-web-devicons', opt = false }
 	}
 
 	--Color scheme
