6076d3d63ed88911c370bf005fa31c6797e4cd41
Luis Aranda <luaranda@microsoft.com>
2023-09-29 17:26:34 -0600

    Add Which key documentation Fix indent-blankline config

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -43,6 +43,17 @@ local on_attach = function(_, bufnr)
     end, { desc = 'Format current buffer with LSP' })
 end
 
+-- document existing key chains
+require('which-key').register({
+    ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+    ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+    ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+    ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
+    ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+    ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+    ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+})
+
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -62,7 +73,7 @@ local servers = {
         },
     },
     omnisharp = {
-        cmd = { "dotnet", vim.fn.expand "~/.omnisharp/Omnisharp.dll"},
+        cmd = { "dotnet", vim.fn.expand "~/.omnisharp/Omnisharp.dll" },
 
         -- Enables support for roslyn analyzers, code fixes and rulesets.
         enable_roslyn_analysers = true,
@@ -79,7 +90,7 @@ local servers = {
         -- document formatting.
         organize_imports_on_format = true,
 
-        filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props'}
+        filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props' }
     }
 }
 
