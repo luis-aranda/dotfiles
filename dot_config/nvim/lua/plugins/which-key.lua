9eb469eed0cba4690a962ca33023118bb84b5a7b
Luis Aranda <luaranda@microsoft.com>
2024-08-21 00:25:25 -0600

    Update which-key to remove deprecated directive

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -21,7 +21,7 @@ return { -- Useful plugin to show you pending keybinds.
 		require("which-key").setup()
 
 		-- Document existing key chains
-		require("which-key").register({
+		require("which-key").add({
 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
 			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
@@ -30,3 +30,4 @@ return { -- Useful plugin to show you pending keybinds.
 		})
 	end,
 }
+
